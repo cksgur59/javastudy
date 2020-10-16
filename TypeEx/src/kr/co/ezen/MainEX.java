9460b3fdd08d3d775e1d8ff545db4331a765562a
Chan <cksgur59@gmail.com>
2020-10-16 17:44:37 +0900

    2020-10-16 Varialbe study

---
+++ TypeEx/src/kr/co/ezen/MainEX.java
@@ -0,0 +1,118 @@
+package kr.co.ezen;
+
+public class MainEX {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		char c = 'c';
+		byte d = -128;
+		byte e = 127;
+		short f = -32768;
+		short g = 32767;
+		int h = -2100000000;
+		int i = 2100000000;
+		long j = -2900000000L;
+		long k = 2900000000L;
+		float l = -3.14F;
+		double n = 3.1425167;
+		
+		System.out.println("자료형 -> 기본자료형 참조자료형");
+		System.out.println("참조자료형 = 기본자료형을 제외한 자료형");
+		System.out.println("기본자료형은 8가지");
+		System.out.println("boolean,char,byte,short,int,long,float,double");
+		System.out.println("boolean= true , false");
+		System.out.println("char = 한 문자 ''사용");
+		System.out.println("byte = -128~127 사이의 숫자 표현 가능");
+		System.out.println("short존재");
+		System.out.println("int 정수형의 기본 자료형 -21억~21억 사이의 숫자 표현 가눙");
+		System.out.println("long 큰 수자를 표현 뒤에 L을 써줘야 한다");
+		System.out.println("float double보다 덜 정밀한 수에 사용 뒤에 F를 써줘야 한다");
+		System.out.println("double 실수형 자료형의 기본 float 보다 더 정밀한 수에 사용");
+		System.out.println(a);
+		System.out.println(f);
+		
+		boolean aa = true;
+		boolean bb = false;
+		char cc = 'a';
+		byte dd = -128;
+		byte ee = 127;
+		short ff = -32768;
+		short gg = 32767;
+		int hh = -2100000000;
+		int ii = 2100000000;
+		long jj = -2500000000L;
+		long kk = 2500000000L;
+		double ll= -3.15648641;
+		float nn= -3.14F;
+		
+		boolean aaa= true;
+		boolean bbb= false;
+		byte ccc= -128;
+		byte ddd= 127;
+		short eee= -32768;
+		short fff= 32767;
+		int ggg= -2100000000;
+		int hhh= 2100000000;
+		long iii= -2500000000L;
+		long jjj= 2500000000L;
+		double kkk= -3.56489641534564;
+		float lll = 3.14F;
+		
+		String aaaa = new String("d7");
+		String bbbb = new String("good");
+		
+		boolean a1 = true;
+		boolean a2 = false;
+		char a3 = 'a';
+		byte a4 = -128;
+		byte a5 = 127;
+		short a6 = -32768;
+		short a7 = 32767;
+		int a8= -2100000000;
+		int a9 = 2100000000;
+		long a10 = -2500000000L;
+		long a11 = 2600000000L;
+		float a12 = 3.12F;
+		double a13= 3.12312318123;
+		
+		boolean b1 =true;
+		boolean b2 =false;
+		char b3 = 'b';
+		byte b4 = -128;
+		byte b5 = 127;
+		short b6 = -32768;
+		short b7 = 32767;
+		int b8 = -2100000000;
+		int b9 = 2100000000;
+		long b10 = -2500000000L;
+		long b11 = 2500000000L;
+		float b12 = 3.14F;
+		double b13 = 3.12381249192;
+		
+		boolean c1 = true;
+		char c2 = 'a';
+		byte c3 = -128;
+		short c4 = -32768;
+		int c5 = -2100000000;
+		long c6 = 22389298432489L;
+		float c7 = 3.123F;
+		double c8 = 3.1098218213098213;
+		
+		boolean d1 = false;
+		char d2 = 'd';
+		byte d3 = -128;
+		short d4 = -32768;
+		int d5 = 2100000000;
+		long d6 = 9223372030000000000L;
+		float d7 = 3.123456F;
+		double d8 = 3.123456789054321;
+		System.out.println(d6);
+		
+		boolean e1 = false;
+	
+		
+		
+		
+	}
+
+}
---
+++ VariableEX/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,57 @@
+package kr.co.ezen;
+
+public class MainEx {
+	
+	public static void main(String[] args) {
+		String msg = "hello";
+		String msg2= "hi";
+		String msg3= "good";
+		String msg4= "morning";
+		
+		System.out.println(msg);
+		System.out.println(msg2);
+		System.out.println(msg3);
+		System.out.println(msg4);
+		System.out.println(msg);
+		System.out.println(msg3);
+		System.out.println(msg2);
+		System.out.println(msg);
+		
+		String msg5 = "hello";
+		String msg6 = "hi";
+		String msg7 = "good";
+		String msg8 = "morning";
+		String msg9 = "asdasd";
+		
+		System.out.println(msg5);
+		System.out.println(msg6);
+		System.out.println(msg7);
+		System.out.println(msg8);
+		System.out.println(msg7);
+		System.out.println(msg6);
+		System.out.println(msg5);
+		System.out.println(msg9);
+		
+		
+		// 자료형 변수명 = 자료형에 맞는 값;
+		// 값에맞는자료형 변수명 = 값;
+		
+		
+		
+		int a = 550;
+		long aaa = 2500000000L;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
