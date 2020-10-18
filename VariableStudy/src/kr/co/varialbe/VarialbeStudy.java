e34c3152e0d790912aad12b3f7cb9c30e87ce72e
Chan <cksgur59@gmail.com>
2020-10-18 23:04:59 +0900

    2020 10 18 hs Variable

---
+++ VariableStudy/src/kr/co/varialbe/VarialbeStudy.java
@@ -0,0 +1,35 @@
+package kr.co.varialbe;
+
+public class VarialbeStudy {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		boolean a1 = true;
+		boolean a2 = false;
+		char a3 = 'a';
+		byte a4 = -128;
+		byte a5 = 127;
+		short a6 = -32768;
+		short a7 = 32767;
+		int a8 = -2100000000;
+		int a9 = 2100000000;
+		long a10 = 2222222222L;
+		float a11 = 3.15F;
+		double a12 = 3.123123213;
+		
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a5);
+		System.out.println(a6);
+		System.out.println(a7);
+		System.out.println(a8);
+		System.out.println(a9);
+		System.out.println(a10);
+		System.out.println(a11);
+		System.out.println(a12);
+		
+		
+	}
+
+}
