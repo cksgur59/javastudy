78a46223fa5596941a851b3dc2de7389c77cf197
Chan <cksgur59@gmail.com>
2020-10-19 17:51:02 +0900

    2020 10 19 ezen Variable2

---
+++ Hoo/src/com/naver/Dog.java
@@ -0,0 +1,21 @@
+package com.naver;
+
+public class Dog {
+
+	int SR;
+
+	public Dog(int sr) {
+		this.SR = sr;
+	}
+
+	public void carl() {
+		if (SR==1){
+			System.out.println("1. hello");
+		}
+		if (SR==2) {
+			System.out.println("2. godd morning");
+		}
+
+	}
+
+}
---
+++ Hoo/src/com/naver/MainEx.java
@@ -0,0 +1,10 @@
+package com.naver;
+
+public class MainEx {
+	
+	public static void main(String[] args) {
+		Dog dog = new Dog(2);
+		dog.carl();
+	}
+
+}
