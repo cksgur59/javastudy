6144dbd6f1ea3c828c87f7019eea658d87c569a0
Chan <cksgur59@gmail.com>
2020-10-22 17:56:38 +0900

    20201022 ezen Method4

---
+++ MethodTest4-1/src/com/naver/RegeM.java
@@ -0,0 +1,18 @@
+package com.naver;
+
+public class RegeM {
+	
+	public RegeM() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Dog dddd(String hp,int sp) {
+		
+		
+		return new Dog(hp,sp);
+	}
+	
+	
+	
+
+}
---
+++ MethodTest4-1/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,13 @@
+package kr.co.ezen;
+
+import com.naver.Dog;
+import com.naver.RegeM;
+
+public class MainEx {
+	public static void main(String[] args) {
+		RegeM reg = new RegeM();
+		Dog rere = reg.dddd("ȫ�浿", 26);
+		System.out.println(rere.getName());
+	}
+
+}
